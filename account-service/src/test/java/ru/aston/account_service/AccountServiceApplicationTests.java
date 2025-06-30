96d7dad21bf83bd1d291446290528ddc3e43992d
PavelVysotski <pavelve83@mail.ru>
2025-06-30 19:47:42 +0300

    git init

---
+++ account-service/src/test/java/ru/aston/account_service/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aston.account_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
